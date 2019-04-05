f0e5d7068360b4af7264ea47c18f916fec2c4d26
Andres Movilla <andres_movilla1103@hotmail.com>
2019-04-04 21:23:38 -0500

    reverting to step 12

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 
 
-import opc13, opc14, opc15
+import opc13, opc14, opc15, opc16
 
 n = 4
 verbose = True
@@ -9,3 +9,4 @@ verbose = True
 opc13.func(n,verbose)
 opc14.func(n,verbose)
 opc15.func(n,verbose)
+opc16.func(n,verbose)
---
+++ opc16.py
@@ -0,0 +1,30 @@
+def func(digs,verbose):
+  if (verbose):
+    print("\nOPC16")
+    print("Copy of OPC12")
+  
+  import math, time
+  startT = time.time()
+
+  start = (10**(digs-1))+1
+  end = 10**digs
+  c = 0
+
+  for n in range(start,end,2):
+    if (n%10 != 5):
+      isPrime = True
+      for x in range(3, int(math.sqrt(n))+1, 2):
+        if (n%x == 0):
+          isPrime = False
+          break
+
+      if (isPrime):
+        c = c + 1
+  
+  execTime = time.time() - startT
+  
+  if (verbose):
+    print("exectime:",execTime)
+    print("primos: ",c)
+
+  return execTime
