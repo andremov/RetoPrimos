083698d1acfb605f169199f93182a98cceefb2e7
Andres Movilla <andres_movilla1103@hotmail.com>
2019-04-04 21:22:25 -0500

    assume numbers are prime

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 
 
-import opc11, opc12, opc13
+import opc11, opc12, opc13, opc14
 
 n = 4
 verbose = True
@@ -9,3 +9,4 @@ verbose = True
 opc11.func(n,verbose)
 opc12.func(n,verbose)
 opc13.func(n,verbose)
+opc14.func(n,verbose)
---
+++ opc14.py
@@ -0,0 +1,27 @@
+def func(digs,verbose):
+  if (verbose):
+    print("\nOPC14")
+    print("Assuming numbers are prime")
+  
+  import math, time
+  startT = time.time()
+
+  start = (10**(digs-1))+1
+  end = 10**digs
+  c = 0
+
+  for n in range(start,end,2):
+    if (n%10 != 5):
+      c = c + 1
+      for x in range(3, int(n**(1/2))+1, 2):
+        if (n%x == 0):
+          c = c - 1
+          break
+  
+  execTime = time.time() - startT
+  
+  if (verbose):
+    print("exectime:",execTime)
+    print("primos: ",c)
+
+  return execTime
