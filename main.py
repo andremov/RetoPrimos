9131558b6138866a9b12314eaf47e180d7edba1e
Andres Movilla <andres_movilla1103@hotmail.com>
2019-04-04 21:21:01 -0500

    changed main loop back to 'for'

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 
 
 
-import opc05, opc06, opc07, opc08, opc09, opc10
+import opc09, opc10, opc11
 
 n = 4
 verbose = True
 
-opc05.func(n,verbose)
-opc06.func(n,verbose)
-opc07.func(n,verbose)
-opc08.func(n,verbose)
 opc09.func(n,verbose)
 opc10.func(n,verbose)
+opc11.func(n,verbose)
