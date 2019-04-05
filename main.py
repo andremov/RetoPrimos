c3f9a18f4c3e82b4e3381f06253e46c8c606009f
Andres Movilla <andres_movilla1103@hotmail.com>
2019-04-04 21:21:22 -0500

    secondary loop skips evens

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 
 
-import opc09, opc10, opc11
+import opc09, opc10, opc11, opc12
 
 n = 4
 verbose = True
@@ -9,3 +9,4 @@ verbose = True
 opc09.func(n,verbose)
 opc10.func(n,verbose)
 opc11.func(n,verbose)
+opc12.func(n,verbose)
