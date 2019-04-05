e675a59f0534e4cae07701c03ef5dfc7e5497ac4
Andres Movilla <andres_movilla1103@hotmail.com>
2019-04-04 21:24:10 -0500

    3-multiples skips

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 
 
 
-import opc13, opc14, opc15, opc16
+import opc16, opc17
 
 n = 4
 verbose = True
 
-opc13.func(n,verbose)
-opc14.func(n,verbose)
-opc15.func(n,verbose)
 opc16.func(n,verbose)
+opc17.func(n,verbose)
