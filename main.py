50d1c0bd051c1b0051e52fc58b87c085e4096ed3
Andres Movilla <andres_movilla1103@hotmail.com>
2019-04-04 21:21:58 -0500

    replacing sqrt with half power

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 
 
 
-import opc09, opc10, opc11, opc12
+import opc11, opc12, opc13
 
 n = 4
 verbose = True
 
-opc09.func(n,verbose)
-opc10.func(n,verbose)
 opc11.func(n,verbose)
 opc12.func(n,verbose)
+opc13.func(n,verbose)
---
+++ opc13.py
@@ -0,0 +1,30 @@
+def func(digs,verbose):
+  if (verbose):
+    print("\nOPC13")
+    print("Replacing sqrt function with power of half")
+  
+  import math, time
+  startT = time.time()
+
+  start = (10**(digs-1))+1
+  end = 10**digs
+  c = 0
+
+  for n in range(start,end,2):
+    if (n%10 != 5):
+      isPrime = True
+      for x in range(3, int(n**(1/2))+1, 2):
+        if (n%x == 0):
+          isPrime = False
+          break
+
+      if (isPrime):
+        c = c + 1
+  
+  execTime = time.time() - startT
+  
+  if (verbose):
+    print("exectime:",execTime)
+    print("primos: ",c)
+
+  return execTime
